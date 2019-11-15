49e70babe0f431a279e43780927108cb0fed27ea
Nam <namlq@tmh-techlab.vn>
2019-11-15 12:40:40 +0700

    remove console.log

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -8,7 +8,6 @@ const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 const { TABLE_NAME } = process.env;
 
 let arrUser = [];
-console.log("123");
 
 exports.handler = async (event, context) => {
   let connectionData;
