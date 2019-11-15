9ed2363c33336bab243f75adf519d86950218c4b
lion5893 <lion5893@gmail.com>
2019-11-15 16:40:57 +0700

    Update app.js

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -8,7 +8,7 @@ const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 const { TABLE_NAME } = process.env;
 
 let arrUser = [];
-console.log("aae");
+console.log("aaf");
 
 exports.handler = async (event, context) => {
   let connectionData;
